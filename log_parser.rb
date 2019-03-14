7d0015690372136fac625160303efdd887d5ddf4
Dzmitry Auramets <dzmitry_auramets@epbyminw8653.minsk.epam.com>
2019-03-14 16:51:07 +0300

    First step of tests

---
+++ log_parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'pry'
+
+class LogParser
+end
+
+LogParser.call(ARGV[0])
---
+++ spec/parsers/log_parser.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe LogParser do
+  subject { described_class.call(log) }
+
+  let(:log) { 'spec/fixtures/sample.log' }
+  let(:page_views) { { '/help_page/1': 3, '/contact': 2, '/home': 1 } }
+  let(:unique_views) { { '/help_page/1': 2, '/contact': 2, '/home': 1 } }
+
+  it 'return page view count' do
+    expect(subgect.page_views).to eq(page_views)
+  end
+
+  it 'return uniq view count' do
+    expect(subject.unique_views).to eq(unique_views)
+  end
+end
